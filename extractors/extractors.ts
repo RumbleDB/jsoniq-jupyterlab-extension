c6c3e5e74c1ba48779af607e9c1e2b8b1345603a
davidbuzatu-marian <davidm.buzatu@gmail.com>
2024-06-25 15:44:22 +0200

    Refactored extractor

--- extractors/extractors.ts
+++ extractors/extractors.ts
@@ -1,9 +1,20 @@
 import { RegExpForeignCodeExtractor } from "@jupyter-lsp/jupyterlab-lsp";
+import { ILSPCodeExtractorsManager } from "@jupyterlab/lsp";
 
-export const jsoniqExtractor = new RegExpForeignCodeExtractor({
-  language: "jsoniq",
-  pattern: "^%%(jsoniq)( .*?)?\n([^]*)",
-  foreignCaptureGroups: [3],
-  isStandalone: true,
-  fileExtension: "jq",
-});
+export class JSONiqExtractor {
+  private extractorManager: any;
+  constructor(extractorManager: ILSPCodeExtractorsManager) {
+    this.extractorManager = extractorManager;
+  }
+
+  public registerExtractor() {
+    const jsoniqExtractor = new RegExpForeignCodeExtractor({
+      language: "jsoniq",
+      pattern: "^%%(jsoniq)( .*?)?\n([^]*)",
+      foreignCaptureGroups: [3],
+      isStandalone: true,
+      fileExtension: "jq",
+    });
+    this.extractorManager.register(jsoniqExtractor, "python");
+  }
+}
