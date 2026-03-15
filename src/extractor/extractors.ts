96b3446c664b04eae2a3dea6103ecd12b024b685
Jimmy Cai <jimmy@cai.im>
2026-03-15 11:41:16 +0100

    Format code with 4 spaces

--- src/extractor/extractors.ts
+++ src/extractor/extractors.ts
@@ -2,19 +2,19 @@ import { RegExpForeignCodeExtractor } from "@jupyter-lsp/jupyterlab-lsp";
 import { ILSPCodeExtractorsManager } from "@jupyterlab/lsp";
 
 export class JSONiqExtractor {
-  private extractorManager: any;
-  constructor(extractorManager: ILSPCodeExtractorsManager) {
-    this.extractorManager = extractorManager;
-  }
+    private extractorManager: any;
+    constructor(extractorManager: ILSPCodeExtractorsManager) {
+        this.extractorManager = extractorManager;
+    }
 
-  public registerExtractor() {
-    const jsoniqExtractor = new RegExpForeignCodeExtractor({
-      language: "jsoniq",
-      pattern: "^%%(jsoniq)( .*?)?\n([^]*)",
-      foreignCaptureGroups: [3],
-      isStandalone: true,
-      fileExtension: "jq",
-    });
-    this.extractorManager.register(jsoniqExtractor, "python");
-  }
+    public registerExtractor() {
+        const jsoniqExtractor = new RegExpForeignCodeExtractor({
+            language: "jsoniq",
+            pattern: "^%%(jsoniq)( .*?)?\n([^]*)",
+            foreignCaptureGroups: [3],
+            isStandalone: true,
+            fileExtension: "jq",
+        });
+        this.extractorManager.register(jsoniqExtractor, "python");
+    }
 }
