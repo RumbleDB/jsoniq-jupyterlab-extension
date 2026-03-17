3939d93c5f7f165bcb9c74095f79f4a1e7c7a090
Jimmy Cai <jimmy@cai.im>
2026-03-17 10:26:17 +0100

    Make our code-mirror the highest precedence to avoid conflict with jupysql-plugin

--- src/code-mirror/highlight.ts
+++ src/code-mirror/highlight.ts
@@ -1,4 +1,4 @@
-import { Compartment, EditorState, Extension } from "@codemirror/state"
+import { Compartment, EditorState, Extension, Prec } from "@codemirror/state"
 import { jsoniq } from "./language";
 import { JSONIQ_MAGIC_COMMAND } from "../const";
 
@@ -7,22 +7,22 @@ const languageConf = new Compartment;
 /**
  * This function is called for every transaction (change in cell input).
  * If the cell is a JSONiq cell (starting with '%%jsoniq'), then the language is set to JSONiq.
+ * Otherwise, it clears its language selection to allow fallback to other extensions.
  */
 const autoLanguage = EditorState.transactionExtender.of(tr => {
     const isJSONIQ = tr.newDoc.sliceString(0, JSONIQ_MAGIC_COMMAND.length) === JSONIQ_MAGIC_COMMAND;
 
-    if (isJSONIQ) {
-        return {
-            effects: languageConf.reconfigure(jsoniq)
-        };
-    }
-
-    return {};
+    return {
+        effects: languageConf.reconfigure(isJSONIQ ? jsoniq : [])
+    };
 })
 
 export function languageSelection(): Extension {
     return [
-        languageConf.of(jsoniq),
+        // We use high precedence to ensure JSONiq wins over other plugins (like jupysql)
+        // when the cell is explicitly marked as JSONiq.
+        // When not JSONiq, we reconfigure to an empty extension [], letting others take over.
+        Prec.highest(languageConf.of([])),
         autoLanguage,
     ];
 }
