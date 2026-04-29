2e386e12f9f013dca627f0cc804c3b179bcaf03d
Jimmy Cai <jimmy@cai.im>
2026-04-29 20:35:36 +0200

    Initial JSONiq language server integration via jupyter-lsp

---
+++ jsoniq_jupyterlab_extension/specs.py
@@ -0,0 +1,23 @@
+from jupyter_lsp.specs.utils import NodeModuleSpec
+
+
+class JSONIQLanguageServer(NodeModuleSpec):
+    node_module = key = "@jimmycai/jsoniq-language-server"
+    script = ["dist", "server", "main.js"]
+    languages = ["jsoniq"]
+    args = ["--stdio"]
+    spec = dict(
+        display_name="JSONiq Language Server",
+        mime_types=["text/x-jsoniq"],
+        urls=dict(
+            home="https://www.npmjs.com/package/@jimmycai/jsoniq-language-server",
+        ),
+        install=dict(
+            npm="npm install -g @jimmycai/jsoniq-language-server",
+            yarn="yarn global add @jimmycai/jsoniq-language-server",
+            pnpm="pnpm add -g @jimmycai/jsoniq-language-server",
+        ),
+    )
+
+
+jsoniq_language_server = JSONIQLanguageServer()
