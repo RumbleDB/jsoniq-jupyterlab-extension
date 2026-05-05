1e7c5ae4c7e52ad7250faedc5f123bd908a20c05
Jimmy Cai <jimmy@cai.im>
2026-05-05 10:41:46 +0200

    Migrate from Jest to Vitest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    moduleFileExtensions: ["js", "ts"],
-    moduleNameMapper: {
-        "^(\\.\\.?\\/.+)\\.js$": "$1",
-    },
-};
--- tests/tokenizier.test.ts
+++ tests/tokenizier.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, test, expect } from "vitest";
 import { TokenToCodeMirrorStyleConverter } from "../src/code-mirror/tokenizer";
 import { jsoniqLexer } from "../src/grammar/jsoniqLexer";
 import { StringStream } from "@codemirror/language";
