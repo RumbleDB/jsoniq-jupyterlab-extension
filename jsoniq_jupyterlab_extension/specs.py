3f912209c4ac77f3b569d8836d672875dc094d46
Jimmy Cai <jimmy@cai.im>
2026-05-08 13:19:31 +0200

    Try to fix `Duplicate filename in local headers` error when releasing Pypi package
    Now language server will be saved to shared folder

--- jsoniq_jupyterlab_extension/specs.py
+++ jsoniq_jupyterlab_extension/specs.py
@@ -1,10 +1,17 @@
 import pathlib
 import shutil
+import sysconfig
 from pathlib import Path
 
 HERE = Path(__file__).parent.resolve()
 REPO_ROOT = HERE.parent
 LANGUAGE_SERVER_ROOT = HERE / "language_server"
+DATA_LANGUAGE_SERVER_ROOT = (
+    Path(sysconfig.get_path("data"))
+    / "share"
+    / "jsoniq-jupyterlab-extension"
+    / "language_server"
+)
 DEV_LANGUAGE_SERVER_ROOT = (
     REPO_ROOT / "node_modules" / "@jimmycai" / "jsoniq-language-server"
 )
@@ -20,11 +27,15 @@ def _find_language_server_root():
     if bundled_script.exists():
         return LANGUAGE_SERVER_ROOT
 
+    data_script = DATA_LANGUAGE_SERVER_ROOT / "dist" / "bundled" / "main.mjs"
+    if data_script.exists():
+        return DATA_LANGUAGE_SERVER_ROOT
+
     dev_script = DEV_LANGUAGE_SERVER_ROOT / "dist" / "bundled" / "main.mjs"
     if dev_script.exists():
         return DEV_LANGUAGE_SERVER_ROOT
 
-    return LANGUAGE_SERVER_ROOT
+    return DATA_LANGUAGE_SERVER_ROOT
 
 
 class JSONIQLanguageServer:
@@ -45,7 +56,7 @@ class JSONIQLanguageServer:
         if node is None:
             return []
 
-        return [node, str(self.script), "--stdio"]
+        return [str(node), str(self.script), "--stdio"]
 
     @property
     def spec(self):
